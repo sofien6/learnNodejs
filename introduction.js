87842d7e5dee17af33dcffe9e1f9a639d5061bf7
sofien6 <foufou.hedjazi@gmail.com>
2022-09-27 14:21:32 +0100

    introduction

---
+++ introduction.js
@@ -0,0 +1,13 @@
+/*
+
+Node.js is a cross-platform runtime environment and library for running JavaScript applications outside the browser. It is used for creating server-side and networking web applications. It is open source and free to use. It can be downloaded from this link https://nodejs.org/en/
+
+Many of the basic modules of Node.js are written in JavaScript. Node.js is mostly used to run real-time server applications.
+
+The definition given by its official documentation is as follows:
+
+?Node.js is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.?
+
+Node.js also provides a rich library of various JavaScript modules to simplify the development of web applications.
+
+*/
