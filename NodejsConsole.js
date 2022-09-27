cbaaa84b3f4ea63a1ea3a3ef62ac6f019cdbf0d0
sofien6 <foufou.hedjazi@gmail.com>
2022-09-27 15:00:04 +0100

    nodejs console

---
+++ NodejsConsole.js
@@ -0,0 +1,12 @@
+// Node.js console.log()
+
+console.log('Hello Dzbrain');   
+
+// Node.js console.error() is used to render error message on console.
+
+console.error(new Error('Hell! This is a wrong method.'));  
+
+// Node.js  console.warn() is used to display warning message on console.
+
+const name = 'John';  
+console.warn(`Don't mess with me ${name}! Don't mess with me!`);   
